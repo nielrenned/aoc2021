2ac6f91b1967747e2d8f211be3a318ef4e97efa2
Jacob Dennerlein <jdennerl@vols.utk.edu>
2021-12-24 14:12:11 -0500

    Add Day1.py

---
+++ Day1.py
@@ -0,0 +1,18 @@
+DAY = 1
+RAW_INPUT = None
+INPUT = None
+
+def load_input():
+    global DAY, RAW_INPUT
+    with open(f'inputs/day{DAY}.txt') as f:
+        RAW_INPUT = f.read()
+
+def parse_input():
+    global RAW_INPUT, INPUT
+    pass
+
+def part1():
+    pass
+
+def part2():
+    pass
