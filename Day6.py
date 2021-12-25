76e0083477d11e63468c7c511134a838821a5868
Jacob Dennerlein <jdennerl@vols.utk.edu>
2021-12-25 14:11:42 -0500

    Update Day 6.

--- Day6.py
+++ Day6.py
@@ -12,19 +12,38 @@ def load_input(use_test_input=False):
 
 def parse_input():
     global INPUT
-    pass
+    INPUT = list(map(int, RAW_INPUT.split(',')))
 
 def part1():
-    pass
+    population = INPUT[:]
+    for _ in range(80):
+        new_fish = 0
+        for i in range(len(population)):
+            population[i] -= 1
+            if population[i] < 0:
+                population[i] = 6
+                new_fish += 1
+        population = population + [8]*new_fish
+    return len(population)
+            
 
 def part2():
-    pass
+    initial_population = INPUT
+    counts = {i: 0 for i in range(9)}
+    for x in initial_population:
+        counts[x] += 1
+    for _ in range(256):
+        new_counts = {i: counts[i+1] for i in range(8)}
+        new_counts[6] += counts[0]
+        new_counts[8] = counts[0]
+        counts = new_counts
+    return sum(counts[i] for i in range(9))
 
 def main():
     load_input()
     parse_input()
     print('PART 1:', part1())
-    # print('PART 2:', part2())
+    print('PART 2:', part2())
 
 if __name__ == "__main__":
     main()
