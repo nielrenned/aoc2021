83f0ee1ed9a8c64f260214cbb1ba5acc7cf1dede
Jacob Dennerlein <jdennerl@vols.utk.edu>
2021-12-25 13:22:25 -0500

    Day 8 Finished.

--- Day8.py
+++ Day8.py
@@ -13,12 +13,11 @@ def load_input(use_test_input=False):
 def parse_input():
     global INPUT
     INPUT = []
-    for line in RAW_INPUT.split('\n')[:1]:
+    for line in RAW_INPUT.split('\n')[:-1]:
         signal, output = line.split(' | ')
         signal = tuple(signal.split())
         output = tuple(output.split())
         INPUT.append((signal, output))
-        
 
 def part1():
     total = 0
@@ -31,14 +30,43 @@ def part1():
                 total += 1
     return total
 
+from functools import reduce
+
 def part2():
-    pass
+    total = 0
+    for signal, output in INPUT:
+        sets = list(map(set, sorted(signal, key=len)))
+        
+        segs = {}
+        segs['a'] = sets[1] - sets[0]
+        segs['d'] = sets[3] & sets[4] & sets[5] & (sets[2] - sets[0])
+        segs['b'] = sets[2] - segs['d'] - sets[0]
+        segs['g'] = (sets[3] & sets[4] & sets[5]) - segs['a'] - segs['d']
+        segs['f'] = (sets[6] & sets[7] & sets[8]) -  segs['a'] - segs['b'] - segs['g']
+        segs['c'] = sets[0] - segs['f']
+        segs['e'] = sets[-1] - segs['a'] - segs['b'] - segs['c'] - segs['d'] - segs['f'] - segs['g']
+        
+        digits = {}
+        digits[frozenset(reduce(lambda x, y: x | y, (segs[x] for x in 'abcefg')))]  = '0'
+        digits[frozenset(reduce(lambda x, y: x | y, (segs[x] for x in 'cf')))]      = '1'
+        digits[frozenset(reduce(lambda x, y: x | y, (segs[x] for x in 'acdeg')))]   = '2'
+        digits[frozenset(reduce(lambda x, y: x | y, (segs[x] for x in 'acdfg')))]   = '3'
+        digits[frozenset(reduce(lambda x, y: x | y, (segs[x] for x in 'bcdf')))]    = '4'
+        digits[frozenset(reduce(lambda x, y: x | y, (segs[x] for x in 'abdfg')))]   = '5'
+        digits[frozenset(reduce(lambda x, y: x | y, (segs[x] for x in 'abdefg')))]  = '6'
+        digits[frozenset(reduce(lambda x, y: x | y, (segs[x] for x in 'acf')))]     = '7'
+        digits[frozenset(reduce(lambda x, y: x | y, (segs[x] for x in 'abcdefg')))] = '8'
+        digits[frozenset(reduce(lambda x, y: x | y, (segs[x] for x in 'abcdfg')))]  = '9'
+        
+        total += int(reduce(lambda x, y: x+y, (digits[frozenset(x)] for x in output)))
+    return total
+
 
 def main():
-    load_input(True)
+    load_input()
     parse_input()
     print('PART 1:', part1())
-    # print('PART 2:', part2())
+    print('PART 2:', part2())
 
 if __name__ == "__main__":
     main()
